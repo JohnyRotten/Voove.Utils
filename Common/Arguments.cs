e4a0c2f7bcb47cb5c69e0c7ba143b474c28f7c39
Oleg Petrunkin <Oleg Petrunkin>
2017-03-05 13:55:57 +0300

    Initial commit

---
+++ Common/Arguments.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public class Arguments
+    {
+        private readonly List<string> _singleArgs;
+        private readonly Dictionary<string, string> _valuesArgs;
+
+        public Arguments(params string[] args)
+        {
+            _singleArgs = new List<string>();
+            _valuesArgs = new Dictionary<string, string>();
+            foreach (var arg in args)
+            {
+                if (IsKeyedArg(arg))
+                {
+                    var arr = arg.Split('=');
+                    _valuesArgs[arr[0].ToUpper().Substring(1)] = arg.Substring(arr.Length + 1);
+                }
+                else
+                {
+                    _singleArgs.Add(arg);
+                }
+            }
+        }
+
+        public int Length => _singleArgs.Count + _valuesArgs.Count;
+
+        public string this[string key] => _valuesArgs[key.ToUpper()];
+        public string this[int index] => _singleArgs[index];
+        public bool HasKey(string key) => _valuesArgs.ContainsKey(key.ToUpper());
+
+        private bool IsKeyedArg(string arg) => arg.StartsWith("/") || arg.StartsWith("-");
+
+    }
+}
---
+++ WebChecker/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace WebChecker
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var arguments = new Arguments(args);
+                var content = GetContent(new Uri(arguments[0]));
+                Console.WriteLine(content);
+                if (arguments.HasKey("r"))
+                {
+                    Console.WriteLine(SearchByRegex(content, arguments["r"]));
+                }
+                else if (arguments.HasKey("s"))
+                {
+                    Console.WriteLine(Search(content, arguments["s"]));
+                }
+                else
+                {
+                    throw new Exception("Invalid arguments.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}{Environment.NewLine}Usage: WebChecker url [-s=search|-r=regex]");
+            }
+        }
+
+        private static bool Search(string content, string search) =>
+            content.ToUpper().Contains(search.ToUpper());
+
+        private static bool SearchByRegex(string content, string search) =>
+            new Regex(search, RegexOptions.IgnoreCase).IsMatch(content);
+
+        private static string GetContent(Uri url)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            request.Method = "GET";
+            using (var responce = request.GetResponse())
+            {
+                using (var stream = responce.GetResponseStream())
+                {
+                    if (stream == null) return string.Empty;
+                    using (var reader = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+    }
+}
